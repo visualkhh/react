3b1a156cf990d0bf84e91a5966ef5108d7390038
visualkhh <visualkhh@gmail.cpm>
2022-05-17 10:43:44 +0900

    redux

--- examples/nextjs-typescript/hooks/index.ts
+++ examples/nextjs-typescript/hooks/index.ts
@@ -1,6 +1,9 @@
-import {Dispatch, EffectCallback, SetStateAction, useEffect, useState} from "react";
+import {Dispatch, EffectCallback, useCallback, useMemo, SetStateAction, useEffect, useState} from "react";
 import {Observable} from "rxjs";
 import {StateValue} from "./state/StateValue";
+import {Slice, SliceCaseReducers} from "@reduxjs/toolkit";
+import {useDispatch, useSelector} from "react-redux";
+import {AppDispatch} from "../redux-store";
 export const makeEffectObservable = <T>(observable: Observable<T>) => {
     const r = {
         subscribe: (next: (value: T) => void) => {
@@ -37,4 +40,59 @@ export const makeStatePropertys = <T>(obj: T): T => {
         })
     });
     return rObj;
+}
+
+
+export const useOneEffect = (effect: EffectCallback) => {
+    useEffect(effect, []);
+}
+export const useOneCallback = <T extends Function>(callBack: T) => {
+    return useCallback(callBack, []);
+}
+export const useOneMemo = <T>(factory: () => T) => {
+    return useMemo(factory, []);
+}
+
+export const useOneMemoEffectDestroy = <T>(factory: () => T, destroyEffect: (it: T) => void) => {
+    const t = useMemo(factory, []);
+    useEffect(() => {
+        return () => {destroyEffect(t)};
+    }, [])
+    return t;
+}
+export const useOneMemoEffect = <T>(factory: () => T, effect: (it: T) => () => void) => {
+    const t = useMemo(factory, []);
+    useEffect(() => effect(t), [])
+    return t;
+}
+
+export const useStateValue = <T>(initialState: T): {value: T} => {
+    const [data, setData] = useState(initialState);
+    const rObj = {value: data, __data__value: data, __setData__value: setData};
+    Object.defineProperty(rObj, 'value', {
+        get(): any {
+            return rObj[`__data__value`];
+        },
+        set(v: any) {
+            return rObj[`__setData__value`](v);
+        }
+    })
+    return rObj;
+}
+
+
+export const useReduxSlice = <S, R extends SliceCaseReducers<S> = SliceCaseReducers<S>, N extends string = string>(slice: Slice<S, R, N>) => {
+    const actions = slice.actions;
+    const state = useSelector((states: any) => {
+        return (states as any)[slice.name]
+    }) as S;
+    const rObj = {name: slice.name, state}
+    const dispatch = useDispatch<AppDispatch>();
+    for (const actionsKey in actions) {
+        const action = actions[actionsKey];
+        (rObj as any)[actionsKey] = (...props: any[]) => {
+            dispatch((action as any)(...props));
+        }
+    }
+    return rObj as ({state: S, name: N} & typeof actions);
 }
--- examples/ts-test/src/index.ts
+++ examples/ts-test/src/index.ts
@@ -1,6 +1,28 @@
 import {getClient, getClients} from './client';
 
-setInterval(() => {
-    let client = getClients();
-    console.log('-22-', client);
-}, 1000);
+// setInterval(() => {
+//     let client = getClients();
+//     console.log('-22-', client);
+// }, 1000);
+
+
+const test = <T>(param: T, property: keyof T) => {
+    console.log(param[property])
+}
+
+
+type AType = {
+    a: string
+}
+// type AType = {
+//     [key: string]: string;
+// }
+
+
+const a: AType = {
+    a: 'a33333'
+}
+
+test(a, 'a');
+
+
